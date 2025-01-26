3cdbe8cad6e5e9773be3ea938832f31f034df18f
Pundah <36124664+Pundah@users.noreply.github.com>
2025-01-26 11:59:29 -0700

    Create QB-Shop Template.lua

---
+++ _Templates/QB-Shop Template.lua
@@ -0,0 +1,14 @@
+----------------------USE ME IF AT A CLOTHING SHOP---------------------
+['grade level'] = 0 -- The Rank
+['outfit label'] = "placeholder" -- Ex Long Sleeve
+['arms'] = {item = 0, texture = 0,}, -- Arms
+['t-shirt'] = {item = 0, texture = 0}, -- Undershirt/Belts
+['torso2'] = {item = 0, texture = 0}, -- Jackets/Tops
+['vest'] = {item = 0, texture = 0}, -- Vest
+['accessory'] = {item = 0, texture = 0}, -- Neck Accessory
+['bag'] = {item = 0, texture = 0}, -- Bag
+['pants'] = {item = 0, texture = 0}, -- Pants
+['shoes'] = {item = 0, texture = 0}, -- Shoes
+['mask'] = {item = 0, texture = 0} -- Mask
+['hat'] = {item = -1, texture = 0}, -- Hat
+['glass'] = {item = 0, texture = 0}, -- Glasses
