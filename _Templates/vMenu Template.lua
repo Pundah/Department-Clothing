a162fc84bb87b156264a863c146a7c0bb6770da1
Pundah <36124664+Pundah@users.noreply.github.com>
2025-01-26 11:59:39 -0700

    Create vMenu Template.lua

---
+++ _Templates/vMenu Template.lua
@@ -0,0 +1,14 @@
+----------------------USE ME IF USING VMENU----------------------------
+['grade level'] = 0 -- The Rank
+['outfit label'] = "placeholder" -- Ex Long Sleeve
+['arms'] = {item = 0, texture = 0,}, -- Hands / Upper Body
+['pants'] = {item = 0, texture = 0}, -- Legs / Pants
+['bag'] = {item = 0, texture = 0}, -- Bag / Parachutes
+['shoes'] = {item = 0, texture = 0}, -- Shoes
+['accessory'] = {item = 0, texture = 0}, -- Neck / Scarfs
+['t-shirt'] = {item = 0, texture = 0}, -- Undershirt/Belts
+['vest'] = {item = 0, texture = 0}, -- Body Armor / Accessory
+['torso2'] = {item = 0, texture = 0}, -- Shirt Overlay / Jackets
+['hat'] = {item = 0, texture = 0}, -- Hat / Helmets
+['glass'] = {item = 0, texture = 0}, -- Glasses
+['mask'] = {item = 0, texture = 0} -- Mask
